d75983d386b2678e87d68a55162c4441af1bf753
Nitsan Technologies <sanjay@nitsan.in>
2018-12-14 19:02:44 +0530

    Initial Upload Of Extension

---
+++ Classes/Controller/NSnowsController.php
@@ -0,0 +1,90 @@
+<?php
+namespace Nitsan\NsSnow\Controller;
+
+/***
+ *
+ * This file is part of the "NS Snow" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2018
+ *
+ ***/
+
+/**
+ * NSnowsController
+ */
+class NSnowsController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
+
+    /**
+     * action list
+     *
+     * @return void
+     */
+    public function listAction()
+    {
+        $flackcount     =   $this->settings['flackcount'];
+        $activeflackimg =   $this->settings['activeflackimg'];
+        $flackimg       =   $this->settings['flackimg'];
+        $flackcolor     =   $this->settings['flackcolor'];
+        $minflacksize   =   $this->settings['minflacksize'];
+        $maxflacksize   =   $this->settings['maxflacksize'];
+        $minflackspeed  =   $this->settings['minflackspeed'];
+        $maxflackspeed  =   $this->settings['maxflackspeed'];
+        $roundflack     =   $this->settings['roundflack'];
+        $shadowflack    =   $this->settings['shadowflack'];
+        $disablesnow    =   $this->settings['disablesnow'];    
+        $desktoponly    =   $this->settings['desktoponly'];
+
+        if ($disablesnow) {
+        } else {            
+            if ($activeflackimg) {
+                if($desktoponly) {
+                       $GLOBALS['TSFE']->additionalFooterData['ns_snow'] .= "<script>
+                                $(document).ready(function(){
+                                    if ($(window).width() > 768) {
+                                        $(document).snowfall();
+                                        $('.collectonme').hide();
+                                        $(document).snowfall('clear');                
+                                        $(document).snowfall({image :'".$flackimg."', minSize: ".$minflacksize.", maxSize:".$maxflacksize.",minSpeed: ".$minflackspeed.", maxSpeed: ".$maxflackspeed.",flakeCount:".$flackcount."});
+                                    }                      
+                                });
+                        </script>";
+                } else {                    
+                     $GLOBALS['TSFE']->additionalFooterData['ns_snow'] .= "<script>
+                        $(document).ready(function(){
+                            $(document).snowfall();
+                            $('.collectonme').hide();
+                            $(document).snowfall('clear');                
+                            $(document).snowfall({image :'".$flackimg."', minSize: ".$minflacksize.", maxSize:".$maxflacksize.",minSpeed: ".$minflackspeed.", maxSpeed: ".$maxflackspeed.",flakeCount:".$flackcount."});
+                        });
+                    </script>";           
+                }
+            } else {
+                if ($desktoponly) {
+                       $GLOBALS['TSFE']->additionalFooterData['ns_snow'] .= "<script>  
+                            $(document).ready(function(){                    
+                                if ($(window).width() > 768) {
+                                    $(document).snowfall();
+                                    $('.collectonme').hide();
+                                    $(document).snowfall('clear');
+                                    $(document).snowfall({shadow : ".$shadowflack.", round : ".$roundflack.", flakeColor:'".$flackcolor."',  minSize: ".$minflacksize.", maxSize:".$maxflacksize.",minSpeed: ".$minflackspeed.", maxSpeed: ".$maxflackspeed.", flakeCount:".$flackcount."});
+                                }                    
+                            });
+                        </script>";
+                } else {
+                    $GLOBALS['TSFE']->additionalFooterData['ns_snow'] .= "<script>  
+                        $(document).ready(function(){                    
+                            $(document).snowfall();
+                            $('.collectonme').hide();
+                            $(document).snowfall('clear');
+                            $(document).snowfall({shadow : ".$shadowflack.", round : ".$roundflack.", flakeColor:'".$flackcolor."',  minSize: ".$minflacksize.", maxSize:".$maxflacksize.", minSpeed: ".$minflackspeed.", maxSpeed: ".$maxflackspeed.", flakeCount:".$flackcount."});
+                        });
+                    </script>";
+                }
+            }        
+        }
+    }
+}
---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3_MODE') or die();
+$extKey = 'ns_snow';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'NS Snow');
