1c4888dacfc005a5cf1137dc51dc18ea87488010
NITSAN Technologies <sanjay@nitsan.in>
2020-02-12 15:46:19 +0530

    [Update] Maintenance Release with TYPO3 v10 Support

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Snow Fall Plugin',
-    'description' => 'Christmas Snow spreads the cheer of Christmas by adding beautiful snowflakes and snow-fall animation to your TYPO3 website with our little addon ns_snow and all the pages will show falling flakes of snow! Live-Demo: https://demo.t3terminal.com/t3t-extensions/snow/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-snowfall-plugin-free/',
+    'description' => 'Christmas Snow spreads the cheer of Christmas by adding beautiful snowflakes and snow-fall animation to your TYPO3 website with our little addon ns_snow and all the pages will show falling flakes of snow! LIVE Demo: https://demo.t3terminal.com/t3t-extensions/snow/ PRO version: https://t3terminal.com/snowfall-typo3-extension-free',
     'category' => 'plugin',
     'author' => 'T3:Milan Rathod, T3:Keval Pandya, QA:Siddharth Sheth',
 	'author_email' => 'sanjay@nitsan.in',
@@ -12,10 +12,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.0',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.0.0-9.5.99',
+            'typo3' => '6.0.0-10.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
