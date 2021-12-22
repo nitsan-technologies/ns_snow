9a51c7d715911d8bf05d0d99c7d80df481d92b25
Stephan Neudecker <nitsan-gitlab@fischer-and-friends.de>
2021-12-22 19:53:26 +0530

    [TODO] - v11 compatiblity

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,19 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
+if (version_compare(TYPO3_branch, '11.0', '>=')) {
+    $moduleClass = \Nitsan\NsSnow\Controller\NSnowsController::class;
+} else {
+    $moduleClass = 'NSnows';
+}
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Nitsan.NsSnow',
     'Nssnows',
     [
-        'NSnows' => 'list'
+        $moduleClass => 'list'
     ],
-    // non-cacheable actions
     [
-        'NSnows' => ''
+        $moduleClass => ''
     ]
 );
