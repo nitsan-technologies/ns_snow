05dbde12ba82855102574f7ae35022199183c2b2
NITSAN <info@nitsan.in>
2025-06-25 12:54:34 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,12 @@
 <?php
 
 $EM_CONF['ns_snow'] = [
-    'title' => 'Snow Fall',
-    'description' => 'Spread the joy of Christmas on your TYPO3 website with the Snowfall TYPO3 Extension. This addon, ns_snow, adds beautiful snowflakes and snowfall animation to all pages, creating a festive atmosphere. TYPO3 Snowfall Extension is also compatible with the latest TYPO3 Version 12. *** Live-Demo: https://demo.t3planet.com/t3-extensions/snow/ *** Documentation & Free Support: https://t3planet.com/typo3-snowfall-extension',
+    'title' => 'TYPO3 Snowfall Animation',
+    'description' => 'Add festive snowfall animations to your TYPO3 website with this lightweight extension. Enhance the holiday mood with animated snowflakes across all pages. Fully compatible with TYPO3 v12 and v13.',
     'category' => 'plugin',
-    'author' => 'T3: Nilesh Malankiya, T3: Himanshu Ramavat, QA: Krishna Dhapa',
-    'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'T3Planet // NITSAN',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
